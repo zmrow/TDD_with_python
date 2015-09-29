8cd6c9b153324fa86d4d34315981d400651fc6b2
Zac Mrowicki <zmrowicki@hotmail.com>
2015-09-28 17:13:47 -0700

    Add helper method to FT to check whether row exists in todo table

--- functional_tests.py
+++ functional_tests.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
   def tearDown(self):
       self.browser.quit()
 
+  def check_for_row_in_list_table(self, row_text):
+      table = self.browser.find_element_by_id('id_list_table')
+      rows = table.find_elements_by_tag_name('tr')
+      self.assertIn(row_text, [row.text for row in rows])
+
   def test_can_start_a_list_and_retrieve_it_later(self):
       # Go to the homepage to check out the app
       self.browser.get('http://localhost:8000')
@@ -33,10 +38,7 @@ class NewVisitorTest(unittest.TestCase):
       # When you hit enter, the page updates and now the page lists
       # "1: Buy peacock feathers" as an item in a to-do list
       inputbox.send_keys(Keys.ENTER)
-
-      table = self.browser.find_element_by_id('id_list_table')
-      rows = table.find_elements_by_tag_name('tr')
-      self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+      self.check_for_row_in_list_table('1: Buy peacock feathers')
 
       # There is still a text box inviting you to add another item.  Enter
       # "Use peacock feathers to make a fly"
@@ -45,13 +47,8 @@ class NewVisitorTest(unittest.TestCase):
       inputbox.send_keys(Keys.ENTER)
 
       # Page updates again, and shows both items on the list
-      table = self.browser.find_element_by_id('id_list_table')
-      rows = table.find_elements_by_tag_name('tr')
-      self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-      self.assertIn(
-          '2: Use peacock feathers to make a fly' ,
-          [row.text for row in rows]
-      )
+      self.check_for_row_in_list_table('1: Buy peacock feathers')
+      self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
       # Will the site remember the list?  The site should generate a unique
       # url for you -- and there is some text to that effect
